878187a80d7e6cdaca16c601904c9d090482d29d
Andrea Rinaldi <andrea.rinaldi@spot.it>
2017-01-24 16:55:08 +0100

    login by google - http call with legacy HttpClient just for testing node API

--- app/src/main/java/it/spot/android/timespot/LoginActivity.java
+++ app/src/main/java/it/spot/android/timespot/LoginActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.auth.api.Auth;
@@ -15,6 +16,19 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import it.spot.android.timespot.databinding.ActivityLoginBinding;
 
@@ -43,6 +57,7 @@ public class LoginActivity
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
         GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("328110632119-votutohgtdhcb7qlao6t07vn0njcfu7s.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
 
@@ -60,9 +75,31 @@ public class LoginActivity
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == REQUEST_CODE_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+           final GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if (result.isSuccess()) {
-                Snackbar.make(mBinding.getRoot(), "Welcome " + result.getSignInAccount().getDisplayName(), Snackbar.LENGTH_LONG).show();
+                Snackbar.make(mBinding.getRoot(), "Welcome " + result.getSignInAccount().getIdToken(), Snackbar.LENGTH_LONG).show();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        HttpClient httpClient = new DefaultHttpClient();
+                        HttpPost httpPost = new HttpPost("http://192.168.100.71:9000/api/session/google/validate");
+                        try {
+                            List nameValuePairs = new ArrayList(1);
+                            nameValuePairs.add(new BasicNameValuePair("id_token", result.getSignInAccount().getIdToken()));
+                            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                            HttpResponse response = httpClient.execute(httpPost);
+                            int statusCode = response.getStatusLine().getStatusCode();
+                            final String responseBody = EntityUtils.toString(response.getEntity());
+                            Log.i("LOGINACTIVITY", "Signed in as: " + responseBody);
+                        } catch (ClientProtocolException e) {
+//                    Log.e(TAG, "Error sending ID token to backend.", e);
+                        } catch (IOException e) {
+//                    Log.e(TAG, "Error sending ID token to backend.", e);
+                        }
+                    }
+                }).start();
             }
         }
     }
