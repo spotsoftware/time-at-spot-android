eb65de7030e3bd1adb87c2d95c0b6a9936cd5f90
Andrea Rinaldi <andrea.rinaldi@spot.it>
2017-01-26 14:26:43 +0100

    looking for the proper way of keeping the cookie up to date

--- app/src/main/java/it/spot/android/timespot/api/TimeEndpoint.java
+++ app/src/main/java/it/spot/android/timespot/api/TimeEndpoint.java
@@ -2,6 +2,7 @@ package it.spot.android.timespot.api;
 
 import android.accounts.Account;
 import android.content.Context;
+import android.util.Log;
 
 import java.io.IOException;
 
@@ -9,6 +10,7 @@ import it.spot.android.timespot.R;
 import it.spot.android.timespot.auth.TimeAuthenticatorHelper;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -21,21 +23,31 @@ public class TimeEndpoint {
     public static Retrofit getInstance(final Context context) {
 
         OkHttpClient httpClient = new OkHttpClient.Builder()
-                .addInterceptor(new Interceptor() {
+                .addNetworkInterceptor(new Interceptor() {
 
                     @Override
                     public Response intercept(Chain chain) throws IOException {
 
                         Account account = TimeAuthenticatorHelper.getAccount(context);
 
+                        Request request = chain.request();
+                        Response response;
                         if (account == null) {
-                            return chain.proceed(chain.request());
+                            response = chain.proceed(request);
 
                         } else {
-                            return chain.proceed(chain.request().newBuilder()
+                            Log.e("TIMEENDPOINT", "added token " + TimeAuthenticatorHelper.getToken(context, account));
+                            response = chain.proceed(request.newBuilder()
                                     .addHeader("Cookie", TimeAuthenticatorHelper.getToken(context, account))
                                     .build());
+
+//                            if (response.headers() != null && account != null) {
+//                                Log.e("TIMEENDPOINT", "updated token " + response.headers().get("Cookie"));
+//                                TimeAuthenticatorHelper.updateToken(context, account, response.headers().get("Cookie"));
+//                            }
                         }
+
+                        return response;
                     }
 
                 }).build();
